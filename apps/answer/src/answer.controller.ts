60baa4587ad874faa4544a5e5128e57f9d5bd316
clr1235 <15035291867@163.com>
2025-08-15 10:41:36 +0800

    feat: 初步尝试微服务

--- apps/answer/src/answer.controller.ts
+++ apps/answer/src/answer.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AnswerService } from './answer.service';
+import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
 
 @Controller()
 export class AnswerController {
   constructor(private readonly answerService: AnswerService) {}
 
+  @Inject('EXAM_SERVICE')
+  private examService: ClientProxy;
+
   @Get()
-  getHello(): string {
-    return this.answerService.getHello();
+  async getHello() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const value = await firstValueFrom(this.examService.send('sum', [1, 3, 5]));
+    return this.answerService.getHello() + value;
   }
 }
--- apps/exam/src/main.ts
+++ apps/exam/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { ExamModule } from './exam.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(ExamModule);
+
+  // 暴露8888的TCP服务
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      port: 8888,
+    },
+  });
+
+  await app.startAllMicroservices();
   await app.listen(process.env.port ?? 3002);
 }
 bootstrap();
