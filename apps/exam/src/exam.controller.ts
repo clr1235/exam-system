369788587440fb0e88ef45047692ab4b61ba89d1
clr1235 <15035291867@163.com>
2025-08-18 10:33:43 +0800

    feat: 开发微服务exam的增删改查接口

--- apps/exam/src/exam.controller.ts
+++ apps/exam/src/exam.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Param, Post, Query } from '@nestjs/common';
 import { ExamService } from './exam.service';
 import { MessagePattern } from '@nestjs/microservices';
 import { RedisService } from '@app/redis';
+import { RequireLogin, UserInfo } from '@app/common';
+import { ExamAddDto } from './dto/exam-add.dto';
+import { ExamSaveDto } from './dto/exam-save.dto';
 
-@Controller()
+@Controller('exam')
 export class ExamController {
   constructor(private readonly examService: ExamService) {}
 
@@ -19,5 +22,35 @@ export class ExamController {
   @MessagePattern('sum')
   sum(numArr: Array<number>): number {
     return numArr.reduce((pre, cur) => pre + cur, 0);
+  }
+
+  @Post('add')
+  @RequireLogin()
+  async add(@Body() dto: ExamAddDto, @UserInfo('userId') userId: number) {
+    return this.examService.add(dto, userId);
+  }
+
+  @Get('list')
+  @RequireLogin()
+  async list(@UserInfo('userId') userId: number, @Query('bin') bin: string) {
+    return this.examService.list(userId, bin);
+  }
+
+  @Post('delete/:id')
+  @RequireLogin()
+  async delete(@UserInfo('userId') userId: number, @Param('id') id: number) {
+    return this.examService.delete(userId, +id);
+  }
+
+  @Post('save')
+  @RequireLogin()
+  async save(@Body() dto: ExamSaveDto) {
+    return this.examService.save(dto);
+  }
+
+  @Post('publish/:id')
+  @RequireLogin()
+  async publish(@UserInfo('userId') userId: number, @Param('id') id: number) {
+    return this.examService.publish(userId, +id);
   }
 }
--- apps/exam/src/exam.module.ts
+++ apps/exam/src/exam.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { ExamController } from './exam.controller';
 import { ExamService } from './exam.service';
 import { RedisModule } from '@app/redis';
+import { APP_GUARD } from '@nestjs/core';
+import { PrismaModule } from '@app/prisma';
+import { CommonModule } from '@app/common';
+import { AuthGuard } from '@app/common/auth.guard';
 
 @Module({
-  imports: [RedisModule],
+  imports: [RedisModule, PrismaModule, CommonModule],
   controllers: [ExamController],
-  providers: [ExamService],
+  providers: [
+    ExamService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class ExamModule {}
