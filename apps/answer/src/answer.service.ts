4e9f2179c0aab20b63b8b598807c1b858dd8595e
clr1235 <15035291867@163.com>
2025-08-19 15:28:39 +0800

    feat: 完成answer相关接口以及excel的lib

--- apps/answer/src/answer.service.ts
+++ apps/answer/src/answer.service.ts
@@ -1,8 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Get,
+  Inject,
+  Injectable,
+  Query,
+} from '@nestjs/common';
+import { AddAnswerDto } from './dto/add-answer.dto';
+import { PrismaService } from '@app/prisma';
+import { ExcelService } from '@app/excel';
 
 @Injectable()
 export class AnswerService {
+  @Inject(PrismaService)
+  private prismaService: PrismaService;
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async add(dto: AddAnswerDto, userId: number) {
+    return await this.prismaService.answer.create({
+      data: {
+        content: dto.content,
+        score: 0,
+        answerer: {
+          connect: {
+            id: userId,
+          },
+        },
+        exam: {
+          connect: {
+            id: dto.examId,
+          },
+        },
+      },
+    });
+  }
+
+  async list(examId: number) {
+    return this.prismaService.answer.findMany({
+      where: {
+        examId,
+      },
+      include: {
+        exam: true,
+        answerer: true,
+      },
+    });
+  }
+
+  async find(id: number) {
+    return this.prismaService.answer.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        exam: true,
+        answerer: true,
+      },
+    });
   }
 }
--- apps/answer/src/main.ts
+++ apps/answer/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AnswerModule } from './answer.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AnswerModule);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.enableCors();
+
   await app.listen(process.env.port ?? 3003);
 }
 bootstrap();
